b2ef1a98e2e29fb4e603f426016ff771d841acf8
PhysGuy01 <franci.specchia@gmail.com>
2023-12-21 22:31:20 +0100

    clean up

--- es4_lez7.cpp
+++ es4_lez7.cpp
@@ -3,28 +3,37 @@
 #include <cmath>
 using namespace std;
 
-const double xC = 1, yC = 1, r = 1;
-
-double distanza(double xP, double yP){
+double distanza(const double xP, const double yP, 
+                const int &xC, const int &yC){
     return sqrt(pow(xP - xC, 2) + pow(yP - yC, 2));
 }
 
-void check_circle(double xP, double yP, bool &status){
-    double z = distanza(xP, yP);
+void check_circle(double xP, double yP, 
+                    int &xC, int &yC, 
+                    int &r, bool &status){
+    double z = distanza(xP, yP, xC, yC);
     if (z < r) status = true;
     else status = false;
 }
 
 int main(){
+    int xC = 1, yC = 1, r = 1;
     bool status;
     double xP, yP;
-    cout << "\nInserire x: ";
-    cin >> xP;
-    cout << "Inserire y: ";
-    cin >> yP;
-    cout << "Distanza dal centro: " << distanza(xP, yP) << endl;
-    check_circle(xP, yP, status);
-    if (status) 
-        cout << "Il punto sta nel cerchio" << endl;
-    else cout << "Il punto non sta nel cerchio" << endl << endl;
+    while (true) {
+        cout << "\nInserire x e y: ";
+        cin >> xP >> yP;
+        if (cin.eof()) break;
+        while(cin.fail()) {
+            cin.clear();
+            cin.ignore(numeric_limits<streamsize>::max(),'\n');
+            cout << "Valore errato, inserisci due NUMERI: ";
+            cin >> xP, yP;
+        } 
+        cout << "\nDistanza dal centro: " << distanza(xP, yP, xC, yC) << endl;
+        check_circle(xP, yP, xC, yC, r, status);
+        if (status) 
+            cout << "Il punto sta nel cerchio" << endl;
+        else cout << "Il punto non sta nel cerchio" << endl << endl;
+    }
 }
